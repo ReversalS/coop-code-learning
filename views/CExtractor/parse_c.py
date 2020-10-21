426864fb591dc8edef688baf0cb55417c8fbb0d4
hzc <aahzcaa@hotmail.com>
2020-10-21 13:23:44 +0800

    c:getpath

--- views/CExtractor/parse_c.py
+++ views/CExtractor/parse_c.py
@@ -4,47 +4,23 @@ import json as json
 
 def parse_file(filename):
     tree = pycparser.parse_file(filename, use_cpp=False)
-
     json_tree = []
 
-    def gen_identifier(identifier, node_type='identifier'):
+    def traverse(node):
         pos = len(json_tree)
         json_node = {}
         json_tree.append(json_node)
-        json_node['type'] = node_type
-        json_node['value'] = identifier
-        return pos
+        json_node['type'] = type(node).__name__
 
-    def traverse(node, node_type=None):
-        def traverse_child(k, v):
-            if v is None:
-                children.append(gen_identifier("NULL", k))
-            elif isinstance(v, list):
-                children.append(traverse(v, k))
-            elif isinstance(v, int) or isinstance(v, float) or isinstance(v, bool) \
-                    or isinstance(v, complex) or isinstance(v, str) or isinstance(v, set):
-                children.append(gen_identifier(str(v), k))
-            else:
-                children.append(traverse(v))
-
-        pos = len(json_tree)
-        json_node = {}
-        json_tree.append(json_node)
-        if node_type is None:
-            json_node['type'] = type(node).__name__
-        else:
-            json_node['type'] = node_type
+        if len(node.attr_names) != 0:
+            json_node['value'] = '#'.join([str(node.__getattribute__(i)) for i in node.attr_names if len(node.__getattribute__(i))!=0])
 
         if node is None:
             return pos
 
         children = []
-        if isinstance(node, list):
-            for l in node:
-                traverse_child(json_node['type'] + '_list', l)
-        else:
-            for k, v in node.children():
-                traverse_child(k, v)
+        for k, v in node.children():
+            children.append(traverse(v))
 
         if len(children) != 0:
             json_node['children'] = children
